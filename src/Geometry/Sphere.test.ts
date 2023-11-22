5f2bdedd403cebfeccf9b2584fe60e6e964506e4
sillyfunnypedro <sillyfunnypedro@gmail.com>
2023-11-22 11:43:45 -0800

    Minimum Sphere test done

--- src/Geometry/Sphere.test.ts
+++ src/Geometry/Sphere.test.ts
@@ -14,16 +14,16 @@ describe('Sphere', () => {
         sphere = new Sphere(radius, center);
     });
 
-    // it('should correctly initialize with a center and radius', () => {
-    //     expect(sphere.center).toEqual(center);
-    //     expect(sphere.radius).toEqual(radius);
-    // });
+    it('should correctly initialize with a center and radius', () => {
+        expect(sphere.center).toEqual(center);
+        expect(sphere.radius).toEqual(radius);
+    });
 
-    // it('should return null intersection when ray does not intersect', () => {
-    //     const ray = new Ray(vec3.fromValues(0, 0, 10), vec3.fromValues(0, 1, 1));
-    //     const intersection = sphere.intersect(ray);
-    //     expect(intersection.hitDistance).toEqual(Number.MAX_VALUE);
-    // });
+    it('should return null intersection when ray does not intersect', () => {
+        const ray = new Ray(vec3.fromValues(0, 0, 10), vec3.fromValues(0, 1, 1));
+        const intersection = sphere.intersect(ray);
+        expect(intersection.hitDistance).toEqual(Number.MAX_VALUE);
+    });
 
     it('should return intersection when ray intersects', () => {
         const ray = new Ray(vec3.fromValues(0, 0, 10), vec3.fromValues(0, 0, -1));
@@ -31,4 +31,17 @@ describe('Sphere', () => {
         expect(intersection.hitDistance).toBeLessThan(Number.MAX_VALUE);
         expect(intersection.hitShape).toEqual(sphere);
     });
+
+    it('should return intersection when the ray his the top of the sphere', () => {
+        const ray = new Ray(vec3.fromValues(0, 13, 12), vec3.fromValues(0, -1, -1));
+        const unitSphere = new Sphere(1, vec3.fromValues(0, 0, 0));
+        const intersection = unitSphere.intersect(ray);
+        const expectedPosition = vec3.fromValues(0, 1, 0);
+        expect(intersection.hitDistance).toBeLessThan(Number.MAX_VALUE);
+        expect(intersection.position).toEqual(expectedPosition);
+        expect(intersection.reflectedRay.direction).toEqual(vec3.fromValues(0, 1, -1));
+        expect(intersection.hitShape).toEqual(unitSphere);
+    });
 });
+
+export { }
